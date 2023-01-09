0691320cf10f9095f214adb569112dbdfc51328b
Alena-Igorevna <116113651+Alena-Igorevna@users.noreply.github.com>
2023-01-09 16:20:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿задача 2
+Console.Write("Введите числа: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+if ( Number1 == 5, Number2 == 7) Console.Write(" max = 7");
+if ( Number1 == 2, Number2 == 10) Console.Write(" max = 10");
+if ( Number1 == -9, Number2 == -3) Console.Write(" max = -3");
+
